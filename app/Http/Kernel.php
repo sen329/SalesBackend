dd28d217a48e1445e6744921815ffdeec4a8b097
sen329 <hans.hilman@gmail.com>
2019-06-10 14:19:34 +0700

    fix cors 4

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        
+        \Barryvdh\Cors\HandleCors::class,
+        //\App\Http\Middleware\Cors::class,
     ];
 
     /**
@@ -41,6 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            //\App\Http\Middleware\Cors::class,
         ],
     ];
 
