e65d477252282504fbbf04da2f2e3a8349a76fc6
sen329 <hans.hilman@gmail.com>
2019-08-29 16:17:29 +0700

    Report have headers

--- app/Exports/ReportsExport.php
+++ app/Exports/ReportsExport.php
@@ -3,15 +3,94 @@
 namespace App\Exports;
 
 use App\report;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class ReportsExport implements FromCollection
+class ReportsExport implements FromQuery, WithHeadings, ShouldAutoSize, WithEvents 
 {
+    use Exportable;
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function query()
     {
-        return report::all()->except(['sales_id'],['order_id']);
+        // return report::all()->except(['sales_id'],['order_id'],['by_userId'],['product_id']);
+        return report::query()->select([
+            'id',
+            'SalesName',
+            'branch',
+            'nolang',
+            'CustomerName',
+            'CustomerAddress',
+            'ContactPerson',
+            'CustomerContact',
+            'ProductCode',
+            'ProductName',
+            'ProductPrice',
+            'LKPP',
+            'ProposedPrice',
+            'Quantity',
+            'Margin',
+            'Total',
+            'Accepted',
+            'Status',
+            'RecommendedPrice',
+            'stock',
+            'TotalRecommendedPrice',
+            'created_at',
+            'updated_at'
+
+        ]);
     }
+
+    public function headings(): array
+    {
+        return [
+            'id',
+            'Sales Name',
+            'Branch',
+            'Nolang',
+            'Customer Name',
+            'Customer Address',
+            'Contact Person',
+            'Phone number',
+            'Product number',
+            'Product name',
+            'COGS',
+            'LKPP',
+            'Proposed Price',
+            'Quantity',
+            'Margin',
+            'Total',
+            'Accepted',
+            'Status',
+            'Recommended Price',
+            'Keterangan',
+            'Total Recommended Price',
+            'Created at',
+            'Updated at'
+
+        ];
+    }
+
+    public function registerEvents(): array
+{
+    return [
+        AfterSheet::class    => function(AfterSheet $event) {
+            // All headers - set font size to 14
+            $cellRange = 'A1:Y1'; 
+            $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(14);
+
+            
+           
+            // Set first row to height 20
+            $event->sheet->getDelegate()->getRowDimension(1)->setRowHeight(20);
+        },
+    ];
+}
+
 }
